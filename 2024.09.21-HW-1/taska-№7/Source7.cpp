c8029d34f1e1fd013e9776461a3945aae5445547
st128271 <st128271@student.spbu.ru>
2024-09-22 12:19:36 +0300

    HW1-s-bogom

---
+++ 2024.09.21-HW-1/taska-№7/Source7.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main(int argc, char* argv[])
+{
+    int v = 0;
+    int t = 0;
+    scanf_s("%d%d", &v, &t);
+    printf("%d", (v*t % 109 + 109) % 109 +1);
+    return EXIT_SUCCESS;
+}
